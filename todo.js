d152ff2dc154b6dced13d15b0a733ed1ea521072
jake <jake@umsun.co.kr>
2018-07-12 17:40:48 +0900

    Add change feature

--- todo.js
+++ todo.js
@@ -1,23 +1,56 @@
+'use strict';
+
+const finish = '완료';
+const unfinish = '미완료';
+
 let count = 0;
 
 function add() {
   let content = document.getElementById('todo').value;
   if (content === '') {
-    alert('할일을 입력해주세요.');
+    alert("할일을 입력해주세요.");
     return;
   }
+  let id = `todo_${count}`;
 
-  let checkBox = `<input type='button' class="toggle" value="do" onclick='toggle("todo_" + ${count});'>`;
-  // let change = `<input type='button' value='수정' onclick='change("list_" + ${count}, );'>`;
-  let remove = `<input type='button' value='삭제' onclick='remove("todo_" + ${count});'>`;
+  let toggleButton = createButton('toggle', finish, function() {
+    toggle(id);
+  });
+  let todoContent = createSpan(content);
+  let changeButton = createButton('change', '수정', function() {
+    change(id);
+  });
+  let removeButton = createButton('remove', '삭제', function() {
+    remove(id);
+  });
 
-  let node = document.createElement("li");
-  node.setAttribute("id", `todo_${count}`);
-  // node.innerHTML = `${checkBox} ${content} ${change} ${remove}`;
-  node.innerHTML = `${checkBox} <span class='todo_text'>${content}</span> ${remove}`;
-  document.getElementById('todo_list').appendChild(node);
+  let li = document.createElement('li');
+  li.setAttribute('id', id);
+
+  li.appendChild(toggleButton);
+  li.appendChild(todoContent);
+  li.appendChild(changeButton);
+  li.appendChild(removeButton);
+
+  document.getElementById("todo_list").appendChild(li);
 
   count++;
+}
+
+function createSpan(content) {
+  let span = document.createElement('span');
+  span.className = 'todo_content'
+  span.innerText = content;
+  return span;
+}
+
+function createButton(c, v, func) {
+  let button = document.createElement('input');
+  button.type = 'button';
+  button.className = c;
+  button.value = v;
+  button.onclick = func;
+  return button;
 }
 
 function remove(id) {
@@ -26,19 +59,21 @@ function remove(id) {
   count--;
 }
 
-function change(id, content) {
+function change(id) {
   let field = document.getElementById(id);
+  let todoContent = field.getElementsByClassName(`todo_content`)[0];
+  todoContent.innerText = prompt("수정해요", todoContent.textContent);
 }
 
 function toggle(id) {
   let field = document.getElementById(id);
-  let toggle = field.getElementsByClassName("toggle")[0];
-  let todoText = field.getElementsByClassName("todo_text")[0];
-  if (toggle.value === "do") {
+  let toggleButton = field.getElementsByClassName(`toggle`)[0];
+  let todoText = field.getElementsByClassName(`todo_content`)[0];
+  if (toggleButton.value === finish) {
     todoText.innerHTML = `<s>${todoText.innerText}</s>`;
-    toggle.value = "undo";
+    toggleButton.value = unfinish;
   } else {
     todoText.innerHTML = `${todoText.innerText}`;
-    toggle.value = "do";
+    toggleButton.value = finish;
   }
 }
